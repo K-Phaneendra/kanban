601e65c0a428f1f622a55f95b92f2b1bcb427592
K-Phaneendra <phaneendra.kosanam@hotmail.com>
2018-10-20 10:52:05 +0530

    added dispatch

--- FrontEnd/src/screens/task/editTaskStatusDnD.js
+++ FrontEnd/src/screens/task/editTaskStatusDnD.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import { find, cloneDeep } from 'lodash';
 import Todo from './dndFiles/todo';
 import Doing from './dndFiles/doing';
 import Done from './dndFiles/done';
+import { updateAssignedTasks } from '../../actions/kanbanToolActions';
 
 const update = require('immutability-helper');
 
 const objectId = '_id';
+const taskStyles = {
+  float: 'left',
+  border: '1px solid',
+  borderStyle: 'dotted',
+  padding: '15px',
+  margin: '5px'
+};
+
+const labelStyles = {
+  textAlign: 'center',
+  textDecoration: 'underline'
+};
 
 class EditTaskStatusDnD extends Component {
   constructor() {
@@ -21,6 +35,27 @@ class EditTaskStatusDnD extends Component {
     const doingData = this.getTasksByStatus(this.props.selectedRow, 'DOING');
     const doneData = this.getTasksByStatus(this.props.selectedRow, 'DONE');
     this.setState({ todoData, doingData, doneData });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.submitClicked) {
+      const tasks = [];
+      this.state.todoData.map(todo => {
+        tasks.push(todo);
+        return null;
+      });
+      this.state.doingData.map(doing => {
+        tasks.push(doing);
+        return null;
+      });
+      this.state.doneData.map(done => {
+        tasks.push(done);
+        return null;
+      });
+      this.props.dispatch(
+        updateAssignedTasks(nextProps.selectedRow[objectId], tasks)
+      );
+    }
   }
 
   getTasksByStatus = (selectedRow, statusName) => {
@@ -112,7 +147,7 @@ class EditTaskStatusDnD extends Component {
     const objectToAdd = find(this.state.doingData, { _id: id }) || find(this.state.doneData, { _id: id });
     console.log('objectToAdd', objectToAdd, id);
     if (objectToAdd) {
-      objectToAdd.status = 'TODO';
+      objectToAdd.status = 'TO DO';
       const toDoDataClone = cloneDeep(this.state.todoData);
       toDoDataClone.push(objectToAdd);
 
@@ -148,7 +183,7 @@ class EditTaskStatusDnD extends Component {
     console.log('objectToAdd', objectToAdd, id);
     if (objectToAdd) {
       objectToAdd.status = 'DONE';
-      const doneDataClone = cloneDeep(this.state.todoData);
+      const doneDataClone = cloneDeep(this.state.doneData);
       doneDataClone.push(objectToAdd);
 
       // delete in doingData
@@ -183,7 +218,8 @@ class EditTaskStatusDnD extends Component {
     console.log('in edit', this.props);
     return (
       <div>
-        <div style={{ float: 'left', border: '1px solid' }}>
+        <div style={taskStyles}>
+        <div style={labelStyles}>TO DO</div>
           {todoData.map((tsk, i) => (
             <Todo
               key={tsk[objectId]}
@@ -195,7 +231,8 @@ class EditTaskStatusDnD extends Component {
             />
           ))}
         </div>
-        <div style={{ float: 'left', border: '1px solid' }}>
+        <div style={taskStyles}>
+            <div style={labelStyles}>DOING</div>
           {doingData.map((tsk, i) => (
             <Doing
               key={tsk[objectId]}
@@ -207,7 +244,8 @@ class EditTaskStatusDnD extends Component {
             />
           ))}
         </div>
-        <div style={{ float: 'left', border: '1px solid' }}>
+        <div style={taskStyles}>
+            <div style={labelStyles}>DONE</div>
           {doneData.map((tsk, i) => (
             <Done
               key={tsk[objectId]}
@@ -224,4 +262,4 @@ class EditTaskStatusDnD extends Component {
   }
 }
 
-export default DragDropContext(HTML5Backend)(EditTaskStatusDnD);
+export default connect()(DragDropContext(HTML5Backend)(EditTaskStatusDnD));
